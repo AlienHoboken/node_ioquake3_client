5562dbac7eea564f068c0e8a72118b50ea798d6c
Jeremiah <AlienHoboken@users.noreply.github.com>
2014-04-30 23:49:25 +0000

    get challenge

--- ioquake3_remote.js
+++ ioquake3_remote.js
@@ -7,17 +7,42 @@ var dgram = require('dgram');
 exports.connect = function(host, port) {
   var sock = dgram.createSocket('udp4');
 
-  sock.bind(port, function() {
+/*  sock.bind(port, function() {
     sock.addMembership(host);
-  });
+  });*/
 
   sock.on('close', function() {
     // TODO emit a server close 
   });
 
-  sock.on('message', function() {
-    // TODO emit a server message event
+  sock.on('message', function(message, rinfo) {
+    //chop off header
+    message = message.slice(4).toString();
+    console.log("Received message: " + message);
+    //check what type it is
+    if(message.indexOf("challengeResponse" === 0)) {
+      //TODO Send clientinfo
+    }
+  });
+
+  //send getchallenge
+  var msg = format("getchallenge");
+  console.log("Sending... " + msg);
+  sock.send(msg, 0, msg.length, port, host, function(err, bytes) {
+    console.log("Sent " + bytes + " bytes. Err: " + err);
   });
 }
 
-
+/*
+   Convenience method. Accepts a string message to send,
+   then turns it into a buffer and supplies the header.
+*/
+format = function(message) {
+  var msg = "xxxx" + message
+  msg = new Buffer(msg);
+  msg[0] = 0xFF;
+  msg[1] = 0xFF;
+  msg[2] = 0xFF;
+  msg[3] = 0xFF;
+  return msg;
+}
